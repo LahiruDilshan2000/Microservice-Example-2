1f61e75077ffd997db5ef32287d232fbe47a8d71
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2026-04-29 10:23:13 +0530

    Add Exception hanlder

--- customer/src/main/java/com/example/microservice/customer/record/CustomerRequest.java
+++ customer/src/main/java/com/example/microservice/customer/record/CustomerRequest.java
@@ -1,9 +1,16 @@
 package com.example.microservice.customer.record;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record CustomerRequest(
         Integer id,
+        @NotBlank(message = "Customer first name is required!")
         String firstName,
+        @NotBlank(message = "Customer last name is required!")
         String lastName,
+        @NotBlank(message = "Customer email is required!")
+        @Email(message = "Customer email is not a valid email address!")
         String email) {
 }
--- customer/src/main/java/com/example/microservice/customer/service/impl/CustomerServiceImpl.java
+++ customer/src/main/java/com/example/microservice/customer/service/impl/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.microservice.customer.service.impl;
 
 import com.example.microservice.customer.Repository.CustomerRepository;
 import com.example.microservice.customer.entity.Customer;
+import com.example.microservice.customer.exception.CustomException;
 import com.example.microservice.customer.record.CustomerRequest;
 import com.example.microservice.customer.record.FraudCheckResponse;
 import com.example.microservice.customer.service.CustomerService;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public ResponseEntity<?> registerCustomer(CustomerRequest request) {
+
+        if(customerRepository.existsByEmail(request.email())){
+            throw new CustomException(
+                   format("Email already exist:: %s", request.email())
+            );
+        }
 
         Customer customer = Customer.builder()
                 .email(request.email())
